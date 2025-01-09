d453ddc927700187e402f82c93b57ebc5cce0cb8
DebiYohana <163828029+DebiYohana@users.noreply.github.com>
2025-01-10 03:40:10 +0700

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -3,9 +3,9 @@ import Hero from "./components/hero";
 import PersonalInfo from "./components/personalinfo";
 import RiwayatPendidikan from "./components/riwayatpendidikan";
 import ContactForm from "./components/contactform";
-import RatingStars from "./components/ratingstars";  
-import Products from "./components/produk";
-
+import RatingStars from "./components/ratingstars";  // Pastikan nama komponen sesuai dengan file
+import "./maul-style.css";  // Pastikan file CSS ada di tempat yang benar
+import HobbiesGallery from "./components/mygallery";
 
 // Fungsi utama untuk render halaman CV online
 export default function CVonline() {
@@ -13,8 +13,8 @@ export default function CVonline() {
     <section>
       <Hero />
       <PersonalInfo />
-      <Products />
       <RiwayatPendidikan />
+      <HobbiesGallery/>
       <ContactForm />
       <RatingStars/>
     </section>
